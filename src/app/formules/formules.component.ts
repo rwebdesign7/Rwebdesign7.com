48ef95d1d16865ec039a5560e6e189e4a9f7eac2
rwebdesign7 <36671112+rwebdesign7@users.noreply.github.com>
2019-05-24 23:04:33 +0200

    Stepper mdb + material non fonctionel

--- src/app/formules/formules.component.ts
+++ src/app/formules/formules.component.ts
@@ -3,7 +3,6 @@ import {Component, OnInit} from '@angular/core';
 import { FormGroup, FormControl, Form} from '@angular/forms';
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
-import { UserInfo} from 'firebase';
 import {FormBuilder, Validators} from '@angular/forms';
 /**
  * @title Stepper vertical
@@ -16,10 +15,11 @@ import {FormBuilder, Validators} from '@angular/forms';
 })
 
 export class FormulesComponent implements OnInit {
+  firstFormGroup: FormGroup;
   private Devis = '/Devis';
   besoins: string[] = ['Site Web', 'Application mobile', 'Les deux', 'autres'];
-  chartes : string[] = ['oui', 'non ' , 'partiellement'];
-  sociales : string[] = ['oui', 'non ' , 'partiellement'];
+  chartes: string[] = ['oui', 'non ' , 'partiellement'];
+  sociales: string[] = ['oui', 'non ' , 'partiellement'];
   modeles: string [] = ['Gratuit avec pubs', 'Application payantes', 'Achats intégrés -in app', 'Gratuit'];
   logos: string[] = ['oui', 'non'];
   cos: string[] = ['oui', 'non'];
@@ -27,51 +27,43 @@ export class FormulesComponent implements OnInit {
   rendus: string[] = ['Normal', 'Avancé', 'Sur mesure'];
   outils: string[] = ['Appareil photo', 'GPS', 'QR CODE', 'NFC/ Bluetooth / WIFI', 'non'];
   currentUser: firebase.User = null;
-  besoin: FormControl;
-  charte: FormControl;
-  modele: FormControl;
-  logo: FormControl;
-  co: FormControl;
-  profil: FormControl;
-  rendu: FormControl;
-  outil: FormControl;
-  noms = FormControl;
-  prénoms = FormControl;
+  besoin = FormControl;
+  charte = FormControl;
+  modele = FormControl;
+  logo = FormControl;
+  co = FormControl;
+  profil = FormControl;
+  rendu = FormControl;
+  outil = FormControl;
   tel = FormControl;
   email = FormControl;
   database = firebase.database();
 
-
-
-  createFormControls() {
-    this.besoin = new FormControl('', Validators.required),
-      this.charte = new FormControl('', Validators.required),
-      this.modele = new FormControl('', Validators.required),
-      this.logo = new FormControl('', Validators.required),
-      // this.noms = new FormControl("", Validators.required),
-      // this.prénoms = new FormControl("", Validators.required),
-      // this.tel = new FormControl("", Validators.required),
-      // this.email = new FormControl("", Validators.required),
-      this.profil = new FormControl('', Validators.required),
-      this.rendu = new FormControl('', Validators.required),
-      this.outil = new FormControl('', Validators.required);
+  constructor ( private router: Router, private fb: FormBuilder) {
 
   }
-
-  createForm() {
+  ngOnInit() {
     this.firstFormGroup = new FormGroup({
-      besoin: this.besoin,
-      charte: this.charte,
-      Modele: this.modele,
-      logo: this.logo,
-      co: this.logo,
-      profil: this.profil,
-      rendu: this.rendu,
-      outil: this.outil,
-      // prénom : this.prénom,
-      // nom : this.nom,
+        besoin: new FormControl(),
+        charte: new FormControl(),
+        modele: new FormControl(),
+        logo: new FormControl(),
+        profil: new FormControl(),
+        rendu: new FormControl(),
+        outil: new FormControl(),
+      });
+    this.firstFormGroup = this.fb.group({
+      besoin: [ '', Validators.required ],
+      charte: [ '', Validators.compose([ Validators.required, Validators.email ]) ],
+      modele: [ '',  ],
+      logo: [ '', Validators.required ],
+      profil: [ '' ],
+      rendu: [ '' ],
+      outil: [ '' ],
     });
   }
+
+
 
 
   // Return an observable list with optional query
@@ -90,24 +82,5 @@ Onsubmit() {
         this.router.navigateByUrl('/home');
       });
     }
-
-  isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  favoriteSeason: string;
-
-
-  constructor(private router: Router, private _formBuilder: FormBuilder) {}
-
-  ngOnInit() {
-    this.createFormControls();
-    this.createForm();
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-  }
 
 }
